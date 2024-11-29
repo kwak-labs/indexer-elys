7147497f243f4c9c02ac1e916921c6e8e039c10d
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2024-11-29 20:36:37 +0100

    fix: v0.53.0 patch 1

--- x/estaking/modules/staking/module.go
+++ x/estaking/modules/staking/module.go
@@ -2,12 +2,13 @@ package staking
 
 import (
 	"context"
-	"cosmossdk.io/core/appmodule"
 	"encoding/json"
 
+	"cosmossdk.io/core/appmodule"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	authcodec "github.com/cosmos/cosmos-sdk/x/auth/codec"
 	"github.com/cosmos/cosmos-sdk/x/staking"
 	"github.com/cosmos/cosmos-sdk/x/staking/exported"
 	"github.com/cosmos/cosmos-sdk/x/staking/keeper"
@@ -38,7 +39,7 @@ type AppModuleBasic struct {
 type AppModule struct {
 	// embed the Cosmos SDK's x/staking AppModule
 	staking.AppModule
-
+	cdc        codec.Codec
 	keeper     *keeper.Keeper
 	accKeeper  types.AccountKeeper
 	bankKeeper types.BankKeeper
@@ -53,6 +54,7 @@ func NewAppModule(cdc codec.Codec, keeper *keeper.Keeper, ak types.AccountKeeper
 		keeper:     keeper,
 		accKeeper:  ak,
 		bankKeeper: bk,
+		cdc:        cdc,
 	}
 }
 
@@ -66,10 +68,110 @@ func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, data json.
 
 // EndBlock delegates the EndBlock call to the underlying x/staking module,
 func (am AppModule) EndBlock(goCtx context.Context) ([]abci.ValidatorUpdate, error) {
+	sdkCtx := sdk.UnwrapSDKContext(goCtx)
+
+	if sdkCtx.BlockHeight() == 11517072 {
+		return am.BlockValidatorUpdates(goCtx)
+	}
+
 	return am.keeper.BlockValidatorUpdates(goCtx)
 }
 
 // BeginBlock returns the begin blocker for the staking module.
 func (am AppModule) BeginBlock(ctx context.Context) error {
 	return am.keeper.BeginBlocker(ctx)
+}
+
+// Skipping UnbondAllMatureValidators
+func (am AppModule) BlockValidatorUpdates(ctx context.Context) ([]abci.ValidatorUpdate, error) {
+	// Calculate validator set changes.
+	//
+	// NOTE: ApplyAndReturnValidatorSetUpdates has to come before
+	// UnbondAllMatureValidatorQueue.
+	// This fixes a bug when the unbonding period is instant (is the case in
+	// some of the tests). The test expected the validator to be completely
+	// unbonded after the Endblocker (go from Bonded -> Unbonding during
+	// ApplyAndReturnValidatorSetUpdates and then Unbonding -> Unbonded during
+	// UnbondAllMatureValidatorQueue).
+	validatorAddressCodec := authcodec.NewBech32Codec(sdk.GetConfig().GetBech32ValidatorAddrPrefix())
+	AddressCodec := authcodec.NewBech32Codec(sdk.GetConfig().GetBech32AccountAddrPrefix())
+	validatorUpdates, err := am.keeper.ApplyAndReturnValidatorSetUpdates(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
+	// Remove all mature unbonding delegations from the ubd queue.
+	matureUnbonds, err := am.keeper.DequeueAllMatureUBDQueue(ctx, sdkCtx.BlockHeader().Time)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, dvPair := range matureUnbonds {
+		addr, err := validatorAddressCodec.StringToBytes(dvPair.ValidatorAddress)
+		if err != nil {
+			return nil, err
+		}
+		delegatorAddress, err := AddressCodec.StringToBytes(dvPair.DelegatorAddress)
+		if err != nil {
+			return nil, err
+		}
+
+		balances, err := am.keeper.CompleteUnbonding(ctx, delegatorAddress, addr)
+		if err != nil {
+			continue
+		}
+
+		sdkCtx.EventManager().EmitEvent(
+			sdk.NewEvent(
+				types.EventTypeCompleteUnbonding,
+				sdk.NewAttribute(sdk.AttributeKeyAmount, balances.String()),
+				sdk.NewAttribute(types.AttributeKeyValidator, dvPair.ValidatorAddress),
+				sdk.NewAttribute(types.AttributeKeyDelegator, dvPair.DelegatorAddress),
+			),
+		)
+	}
+
+	// Remove all mature redelegations from the red queue.
+	matureRedelegations, err := am.keeper.DequeueAllMatureRedelegationQueue(ctx, sdkCtx.BlockHeader().Time)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, dvvTriplet := range matureRedelegations {
+		valSrcAddr, err := validatorAddressCodec.StringToBytes(dvvTriplet.ValidatorSrcAddress)
+		if err != nil {
+			return nil, err
+		}
+		valDstAddr, err := validatorAddressCodec.StringToBytes(dvvTriplet.ValidatorDstAddress)
+		if err != nil {
+			return nil, err
+		}
+		delegatorAddress, err := AddressCodec.StringToBytes(dvvTriplet.DelegatorAddress)
+		if err != nil {
+			return nil, err
+		}
+
+		balances, err := am.keeper.CompleteRedelegation(
+			ctx,
+			delegatorAddress,
+			valSrcAddr,
+			valDstAddr,
+		)
+		if err != nil {
+			continue
+		}
+
+		sdkCtx.EventManager().EmitEvent(
+			sdk.NewEvent(
+				types.EventTypeCompleteRedelegation,
+				sdk.NewAttribute(sdk.AttributeKeyAmount, balances.String()),
+				sdk.NewAttribute(types.AttributeKeyDelegator, dvvTriplet.DelegatorAddress),
+				sdk.NewAttribute(types.AttributeKeySrcValidator, dvvTriplet.ValidatorSrcAddress),
+				sdk.NewAttribute(types.AttributeKeyDstValidator, dvvTriplet.ValidatorDstAddress),
+			),
+		)
+	}
+
+	return validatorUpdates, nil
 }
